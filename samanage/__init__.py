b477e072b1156933b680899b2e65da5c21718b94
Christopher DiTusa <cditusa@gmail.com>
2019-05-17 16:56:17 -0400

    Update to use token authentication

--- samanage/__init__.py
+++ samanage/__init__.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
-import urllib
+import urllib.parse
 import logging
 import argparse
 import requests
@@ -10,7 +10,7 @@ class Record(object):
     def __init__(self, json_payload):
         self.id   = json_payload.get('id', None)
         self.name = json_payload.get('name', None)
-
+    
     def __str__(self):
         return u'{}\n\t'.format(self.name) + '\n\t'.join([u'{}: {}'.format(k,v) 
             for k,v in self.__dict__.items() if v])
@@ -48,30 +48,37 @@ class Incident(Record):
 class User(Record):
     def __init__(self, json_payload):
         super(User, self).__init__(json_payload)
-        self.title      = json_payload.get('title', '')
-        self.department = json_payload.get('department', '')
-        self.email      = json_payload.get('email', '')
+        self.title        = json_payload.get('title', '')
+        self.department   = json_payload.get('department','')
+        self.email        = json_payload.get('email', '')
+        self.mobile_phone = json_payload.get('mobile_phone', '')
+        self.phone        = json_payload.get('phone', '')
+        self.reports_to   = json_payload.get('reports_to', '')
+       #self.site         = json_payload.get('site', '')
+        
+
 
 class Hardware(Record):
     def __init__(self, json_payload):
         super(Hardware, self).__init__(json_payload)
-        self.bio               = [{'ssn': ''}]
         self.address           = json_payload.get('address', '')
         self.asset_tag         = json_payload.get('asset_tag', '')
         self.category          = json_payload.get('category', '')
+        self.created_at        = json_payload.get('created_at' '')
         self.department        = json_payload.get('department', '')
         self.description       = json_payload.get('description', '')
         self.domain            = json_payload.get('domain', '')
         self.ip                = json_payload.get('ip', '')
-        self.latitude          = json_payload.get('latitude', '')
-        self.longitude         = json_payload.get('longitude', '')
         self.networks          = json_payload.get('networks', '')
         self.notes             = json_payload.get('notes', '')
         self.owner             = json_payload.get('owner', '')
+        self.serial_number     = json_payload.get('serial_number', '')
+        self.site              = json_payload.get('site', '')
         self.status            = json_payload.get('status', '')
         self.technical_contact = json_payload.get('technical_contact', '')
+        self.updated_at        = json_payload.get('updated_at', '')
         self.username          = json_payload.get('username', '')
-
+    
     def get_incidents(self, client):
         '''get a list of incidents using the client passed'''
         uri = '{}/hardwares/{}-{}/incidents.json'.format(
@@ -80,30 +87,26 @@ class Hardware(Record):
 
 
 class Samanage(object):
-
     supported_types = {
             'hardwares': Hardware,
             'users': User,
             'departments': Department,
             'catalog_items': CatalogItems,
             'incidents': Incident,
+    }
+
+    def __init__(self, token, uri='https://api.samanage.com'):
+        self.uri             = uri
+        self.token           = token
+        self.session         = requests.Session()
+        self.session.headers = {
+            'X-Samanage-Authorization': 'Bearer {}'.format(token),
+            'Accept': 'application/vnd.samanage.v2.1+json',
+            'Content-Type': 'application/json'
             }
-
-    def __init__(self, username, password, uri='https://api.samanage.com'):
-        self.username     = username
-        self.password     = password
-        self.uri          = uri
-        self.logger       = logging.getLogger('samanage.Samanage')
-        self.session      = requests.Session()
-        self.session.auth = requests.auth.HTTPDigestAuth(
-                self.username, self.password)
-        self.session.headers = { 
-                'Accept'       : 'application/vnd.samanage.v1.2+json',
-                'Content-Type' : 'application/json',
-                }
-        self.logger.debug('using credentials: {}/{}'.format(
-            self.username, self.password))
-
+        self.logger          = logging.getLogger('samanage.Samanage')
+        self.logger.debug('using token: {}'.format(self.token))
+        
     def _uri(self, record_type, record_id=None):
         if record_type not in self.supported_types:
             raise ValueError('{} not supported'.format(record_type))
@@ -116,10 +119,9 @@ class Samanage(object):
         uri = self._uri(record_type, record_id)
         search['per_page'] = count
         if search:
-            uri += '?{}'.format(urllib.urlencode(search))
+            uri += '?{}'.format(urllib.parse.urlencode(search))
             self.logger.debug('add search paramter: {}'.format(uri))
         return uri
-
 
     def _check_response(self, response, record_type):
         results = []
@@ -144,7 +146,7 @@ class Samanage(object):
                 return True
 
     def _get_raw(self, uri, record_type, record_id=None):
-        self.logger.debug('fetching uri:{}'.format(uri))    
+        self.logger.debug('fetching uri:{}'.format(uri))
         response = self.session.get(uri)
         return self._check_response(response, record_type)
 
@@ -159,7 +161,7 @@ class Samanage(object):
 
     def put(self, record_type, payload, record_id):
         if type(record_id) is not int:
-            raise ValueError('record_id must by type int() not {}'.format(
+            raise ValueError('record_id must be type int() not {}'.format(
                 type(record_id)))
         uri = self._uri(record_type, record_id=record_id)
         response = self.session.put(uri, json=self._payload(payload, record_type))
@@ -167,7 +169,7 @@ class Samanage(object):
 
     def delete(self, record_type, record_id):
         if type(record_id) is not int:
-            raise ValueError('record_id must by type int() not {}'.format(
+            raise ValueError('record_id must be type int() not {}'.format(
                 type(record_id)))
         uri = self._uri(record_type, record_id=record_id)
         response = self.session.delete(uri)
@@ -178,23 +180,25 @@ class Samanage(object):
         response = self.session.post(uri, json=self._payload(payload, record_type))
         return self._check_response(response, record_type)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='dns spoof monitoring script')
-    parser.add_argument('-u', '--username', required=True)
-    parser.add_argument('-p', '--password', required=True)
-    parser.add_argument('-T', '--type', required=True)
-    parser.add_argument('-I', '--id', default=None)
-    parser.add_argument('-U', '--uri', default='https://api.samanage.com',
-            help='Sammanage api hendpoint')
-    parser.add_argument('-S', '--search', default='{}', type=json.loads, 
-            help='Search parameters as a hash')
-    parser.add_argument('-C', '--count', default=25, 
-            help='Number of entries to return')
-    parser.add_argument('-v','--verbose', action="count")
+    parser = argparse.ArgumentParser(description='Samanage API script')
+    parser.add_argument('-T', '--token', required=True, type=argparse.FileType('r', encoding='UTF-8'),
+        metavar='FILE', help='Token file to connect to Samanage')
+    parser.add_argument('-t', '--type', required=True)
+    parser.add_argument('-i', '--id', default=None)
+    parser.add_argument('-u', '--uri', default='https://api.samanage.com', 
+        help='Samanage api uri')
+    parser.add_argument('-s', '--search', default='{}', type=json.loads,
+        help='Search parameters as a hash')
+    parser.add_argument('-c', '--count', default=25,
+        help='Number of entries to return')
+    parser.add_argument('-v', '--verbose', action='count')
     args = parser.parse_args()
 
-    log_level = logging.ERROR
-    if args.verbose == 1:
+    if args.verbose is None:
+        log_level = logging.ERROR
+    elif args.verbose == 1:
         log_level = logging.WARN
     elif args.verbose == 2:
         log_level = logging.INFO
@@ -203,11 +207,11 @@ def main():
     logging.basicConfig(level=log_level)
     logger = logging.getLogger('samanage.main')
 
-    client = Samanage(args.username, args.password, args.uri)
+    client = Samanage(args.token.read(), args.uri)
     results = client.get(args.type, args.count, args.id, args.search)
     if results:
         for result in results:
-            print u'{}'.format(result)
+            print(u'{}'.format(result))
 
 
 if __name__ == '__main__':
