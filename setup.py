71e713941fbf939b54fc56f5109078fce4cb3f00
Christopher DiTusa <cditusa@gmail.com>
2019-05-18 16:17:00 -0400

    Edit setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 
 from setuptools import setup
 
-setup(name='samanage',
+with open('README.md') as file:
+      readme = file.read()
+
+with open('LICENSE') as file:
+      license = file.read()
+
+setup(name='samanage3',
       version='1.0',
       description='Library to interface with samanage',
-      author='John Bond',
-      author_email='pypi@johnbond.org',
-      url='https://github.com/b4ldr/samanage',
-      license='BSD',
+      long_description=readme,
+      author='Chris DiTusa (original author: John Bond)',
+      author_email='cditusa@gmail.com',
+      url='https://github.com/toose11/samanage',
+      license=license,
       packages=['samanage'],
      )
